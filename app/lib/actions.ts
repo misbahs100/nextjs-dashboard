aea43b2d112cc77863204bc7e17667290aaa0485
misbahs100 <misbahuddins100@gmail.com>
2024-11-21 12:01:01 +0600

    eslint ignore during builds

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,6 +59,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
+    console.log(error);
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -99,6 +100,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.log(error);
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
  
@@ -107,12 +109,13 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice.')
+  // throw new Error('Failed to Delete Invoice.')
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
   } catch (error) {
+    console.log(error);
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
